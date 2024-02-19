7810a7db4bb749a6395242cddc18527aeabdce0a
Jevon <jevon@MSI>
2024-02-19 23:26:30 +0800

    Add Proj files and source code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from transformers import pipeline
 
 app = FastAPI()
+pipe_t2t = pipeline("text2text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World!"}
+
+@app.get("/infer_tinyllama")
+def t5(input):
+    output = pipe_t2t(input)
+    return {"output": output[0]["generated_text"]}
