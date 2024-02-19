d74f891d3de9fb81bec044c46273eb885adafbeb
Jevon <jevon@MSI>
2024-02-20 02:04:36 +0800

    Update file

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 const textGenForm = document.querySelector(".text-gen-form");
 
 const translateText = async (text) => {
-    const inferResponse = await fetch(`infer_tinyllama?input=${text}`);
+    const inferResponse = await fetch(`infer_t5?input=${text}`);
     const inferJson = await inferResponse.json();
 
     return inferJson.output;
