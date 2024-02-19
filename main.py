ce553741e373e1ad02a3e1da0371707106d7af4f
Jevon <jevon@MSI>
2024-02-20 00:19:49 +0800

    Update Web Codes

--- main.py
+++ main.py
@@ -15,6 +15,5 @@ def t5(input):
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-
 def index() -> FileResponse:
     return FileResponse(path="/app/static/index.html", media_type="text/html")
