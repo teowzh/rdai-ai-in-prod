26e0c12d3247293ef69caed2bafa4eaf5edddc08
Jevon <jevon@MSI>
2024-02-20 01:10:04 +0800

    Update codes

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,6 @@ COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install -U pip
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-RUN pip install git+https://github.com/huggingface/transformers
 
 COPY --chown=user . $HOME/app
 
--- main.py
+++ main.py
@@ -2,13 +2,29 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from transformers import pipeline
+from transformers import AutoTokenizer
+from awq import AutoAWQForCausalLM
+import torch
 
 app = FastAPI()
-pipe_t2t = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World!"}
+original_model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+quant_path = 'TinyLlama-1.1B-Chat-v1.0-awq'
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMV" }
+
+model = AutoAWQForCausalLM.from_pretrained(
+    original_model_id, safetensors=True, **{"torch_dtype": torch.float16, "low_cpu_mem_usage": True, "use_cache": False}, device_map="cuda:0"
+)
+tokenizer = AutoTokenizer.from_pretrained(original_model_id,
+                                          torch_dtype="auto")
+
+model.quantize(tokenizer, quant_config=quant_config)
+# pipe_t2t = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+pipe_t2t = pipeline("text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                torch_dtype=torch.bfloat16,
+                device_map="auto")
 
 @app.get("/infer_tinyllama")
 def tinyllama(input):
